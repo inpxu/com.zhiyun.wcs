5d7cd60b02b3d5197bf391bea30818e34bbd8055
xufei <949547597@qq.com>
2018-10-31 10:13:07 +0800

    2018-10-31 08:04:15

--- src/main/java/com/zhiyun/entity/SitSet.java
+++ src/main/java/com/zhiyun/entity/SitSet.java
@@ -5,11 +5,9 @@
 
 package com.zhiyun.entity;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.zhiyun.base.entity.BaseEntity;
-
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Max;
 
 /**
  * 设备节点设置实体类
@@ -18,108 +16,235 @@ import javax.validation.constraints.Pattern;
  * @version v1.0
  * @date
  */
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SitSet extends BaseEntity<Long> {
 
-    private static final long serialVersionUID = 6296550773028540004L;
+	private static final long serialVersionUID = 7509146345876942897L;
 
-    // ~~~~实体属性
-    // 节点id
-    @Max(value = 9223372036854775807L, message = "节点id字段过长")
-    private Long id;
-    // 设备编码
-    @Pattern(regexp = "[\\s\\S]{0,30}", message = "设备编码字段过长")
-    private String macNo;
-    // 节点名
-    @Pattern(regexp = "[\\s\\S]{0,24}", message = "节点名字段过长")
-    private String siteName;
-    // z
-    private Double z;
-    // y
-    private Double y;
-    // x
-    private Double x;
-    // 偏移量
-    private Double offset;
-    // 节点类型
-    private Integer siteType;
-    // 企业id
-    @Max(value = 9223372036854775807L, message = "企业id字段过长")
-    private Long companyId;
+	// ~~~~实体属性
+	// 设备编码
+	@Pattern(regexp="[\\S]{0,30}", message="设备编码字段过长")
+	private String macNo;
+	// 生产设备编码
+	@Pattern(regexp="[\\S]{0,30}", message="生产设备编码字段过长")
+	private String prodMacNo;
+	// 节点名
+	@Pattern(regexp="[\\S]{0,24}", message="节点名字段过长")
+	private String siteName;
+	// z
+	private Double z;
+	// y
+	private Double y;
+	// x
+	private Double x;
+	// 偏移量
+	private Double offset;
+	// 节点类型节点0,上料1,下料2
+	@Max(value=99999999999L,message="节点类型节点0,上料1,下料2字段过长")
+	private Integer siteType;
+	// 层数
+	@Max(value=99999999999L,message="层数字段过长")
+	private Integer layer;
+	// 设备内置节点编码
+	@Pattern(regexp="[\\S]{0,20}", message="设备内置节点编码字段过长")
+	private String macInSiteNo;
+	// 物料信息
+	@Pattern(regexp="[\\S]{0,30}", message="物料信息字段过长")
+	private String matterName;
+	// 后置时间
+	private java.util.Date afterTime;
+	// 企业id
+	@Max(value=9223372036854775807L,message="企业id字段过长")
+	private Long companyId;
 
-    @Override
-    public Long getId() {
-        return id;
-    }
+	@Override
+	public Long getId() {
+		return super.getId();
+	}
 
-    @Override
-    public void setId(Long id) {
-        this.id = id;
-    }
+	@Override
+	public void setId(Long id) {
+		super.setId(id);
+	}
+	
+	/**
+	 * 设备编码
+	 */
+	public String getMacNo() {
+		return this.macNo;
+	}
 
-    public String getMacNo() {
-        return macNo;
-    }
+	/**
+	 * 设备编码
+	 */
+	public void setMacNo(String macNo) {
+		this.macNo = macNo;
+	}
+	
+	/**
+	 * 生产设备编码
+	 */
+	public String getProdMacNo() {
+		return this.prodMacNo;
+	}
 
-    public void setMacNo(String macNo) {
-        this.macNo = macNo;
-    }
+	/**
+	 * 生产设备编码
+	 */
+	public void setProdMacNo(String prodMacNo) {
+		this.prodMacNo = prodMacNo;
+	}
+	
+	/**
+	 * 节点名
+	 */
+	public String getSiteName() {
+		return this.siteName;
+	}
 
-    public String getSiteName() {
-        return siteName;
-    }
+	/**
+	 * 节点名
+	 */
+	public void setSiteName(String siteName) {
+		this.siteName = siteName;
+	}
+	
+	/**
+	 * z
+	 */
+	public Double getZ() {
+		return this.z;
+	}
 
-    public void setSiteName(String siteName) {
-        this.siteName = siteName;
-    }
+	/**
+	 * z
+	 */
+	public void setZ(Double z) {
+		this.z = z;
+	}
+	
+	/**
+	 * y
+	 */
+	public Double getY() {
+		return this.y;
+	}
 
-    public Double getZ() {
-        return z;
-    }
+	/**
+	 * y
+	 */
+	public void setY(Double y) {
+		this.y = y;
+	}
+	
+	/**
+	 * x
+	 */
+	public Double getX() {
+		return this.x;
+	}
 
-    public void setZ(Double z) {
-        this.z = z;
-    }
+	/**
+	 * x
+	 */
+	public void setX(Double x) {
+		this.x = x;
+	}
+	
+	/**
+	 * 偏移量
+	 */
+	public Double getOffset() {
+		return this.offset;
+	}
 
-    public Double getY() {
-        return y;
-    }
+	/**
+	 * 偏移量
+	 */
+	public void setOffset(Double offset) {
+		this.offset = offset;
+	}
+	
+	/**
+	 * 节点类型节点0,上料1,下料2
+	 */
+	public Integer getSiteType() {
+		return this.siteType;
+	}
 
-    public void setY(Double y) {
-        this.y = y;
-    }
+	/**
+	 * 节点类型节点0,上料1,下料2
+	 */
+	public void setSiteType(Integer siteType) {
+		this.siteType = siteType;
+	}
+	
+	/**
+	 * 层数
+	 */
+	public Integer getLayer() {
+		return this.layer;
+	}
 
-    public Double getX() {
-        return x;
-    }
+	/**
+	 * 层数
+	 */
+	public void setLayer(Integer layer) {
+		this.layer = layer;
+	}
+	
+	/**
+	 * 设备内置节点编码
+	 */
+	public String getMacInSiteNo() {
+		return this.macInSiteNo;
+	}
 
-    public void setX(Double x) {
-        this.x = x;
-    }
+	/**
+	 * 设备内置节点编码
+	 */
+	public void setMacInSiteNo(String macInSiteNo) {
+		this.macInSiteNo = macInSiteNo;
+	}
+	
+	/**
+	 * 物料信息
+	 */
+	public String getMatterName() {
+		return this.matterName;
+	}
 
-    public Double getOffset() {
-        return offset;
-    }
+	/**
+	 * 物料信息
+	 */
+	public void setMatterName(String matterName) {
+		this.matterName = matterName;
+	}
+	
+	/**
+	 * 后置时间
+	 */
+	public java.util.Date getAfterTime() {
+		return this.afterTime;
+	}
 
-    public void setOffset(Double offset) {
-        this.offset = offset;
-    }
+	/**
+	 * 后置时间
+	 */
+	public void setAfterTime(java.util.Date afterTime) {
+		this.afterTime = afterTime;
+	}
+	
+	/**
+	 * 企业id
+	 */
+	public Long getCompanyId() {
+		return this.companyId;
+	}
 
-    public Integer getSiteType() {
-        return siteType;
-    }
-
-    public void setSiteType(Integer siteType) {
-        this.siteType = siteType;
-    }
-
-    @Override
-    public Long getCompanyId() {
-        return companyId;
-    }
-
-    @Override
-    public void setCompanyId(Long companyId) {
-        this.companyId = companyId;
-    }
+	/**
+	 * 企业id
+	 */
+	public void setCompanyId(Long companyId) {
+		this.companyId = companyId;
+	}
 }
