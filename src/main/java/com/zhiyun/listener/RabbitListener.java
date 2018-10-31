8b8c80184eb9ee85bb55ffb0b49eeaae5ba9c7a6
xufei <949547597@qq.com>
2018-10-31 09:26:20 +0800

    2018-10-31 08:04:15

--- src/main/java/com/zhiyun/listener/RabbitListener.java
+++ src/main/java/com/zhiyun/listener/RabbitListener.java
@@ -1,7 +1,10 @@
 package com.zhiyun.listener;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.rabbitmq.client.Channel;
 import com.zhiyun.core.JobProcessEngine;
+import com.zhiyun.entity.SystemTask;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +13,8 @@ import org.springframework.amqp.rabbit.core.ChannelAwareMessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 消息监听器,监听到消息以后将信息添加到任务队列中
@@ -39,7 +44,9 @@ public class RabbitListener implements ChannelAwareMessageListener {
             if (StringUtils.isNotBlank(messageContent)) {
                 //应答mq
                 channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
-                jobProcessEngine.autoShardingJob(messageContent);
+                SystemTask systemTask = JSON.parseObject(messageContent, SystemTask.class);
+
+
             } else {
                 LOGGER.error("消息队列中任务为空");
             }
